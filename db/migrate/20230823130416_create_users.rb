72697c5344106db5c8aeb52cc8dfd816ffbad3d5
forLoop94 <charlesuwem94@gmail.com>
2023-08-23 14:22:17 +0100

    Create migration: comments table, run migration, shema updated

--- db/migrate/20230823130416_create_users.rb
+++ db/migrate/20230823130416_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :photo
-      t.string :bio
+      t.text :photo
+      t.text :bio
       t.integer :post_counter
 
       t.timestamps
