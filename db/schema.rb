fc898d6274c599bd20180bf2b890adbe4378188d
forLoop94 <charlesuwem94@gmail.com>
2023-08-23 14:55:08 +0100

    Add posts reference to comments and add user reference to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_133609) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_134808) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_133609) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,6 +31,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_133609) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,5 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_133609) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
