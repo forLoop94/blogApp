9177b50254aa334cf78edc91d94fb3317d53ceb9
forLoop94 <charlesuwem94@gmail.com>
2023-08-23 14:33:38 +0100

    Add foreign key to Comments table to reference users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_131828) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_23_132654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_131828) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_131828) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users", column: "author_id"
 end
