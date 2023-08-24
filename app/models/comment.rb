26f85cdb900ece46f299ba097704b3ffa34bf32a
forLoop94 <charlesuwem94@gmail.com>
2023-08-24 16:00:00 +0100

    Add required custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, foreign_key: 'post_id'
+
+  def update_comments_count
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
+
+  def recent_comments
+    comments.limit(5).order(created_at: :asc)
+  end
 end
