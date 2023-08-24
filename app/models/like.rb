582f19f9dc5cd4c572ca5aeaf7b5b785989668c5
forLoop94 <charlesuwem94@gmail.com>
2023-08-24 15:19:55 +0100

    test app with rails console, add ApplicationRecord as parent class to Like, Comment and Post

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,4 @@
-class Like
+class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, foreign_key: 'author_id'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
-class Post
+class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
