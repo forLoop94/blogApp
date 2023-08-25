04dc82c482af034fd139bcdd327197142b2e96bc
forLoop94 <charlesuwem94@gmail.com>
2023-08-25 15:37:24 +0100

    In Post model, title is present with max of 250 char; comments and likes counter >= 0

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
   has_many :likes, class_name: 'Like', foreign_key: 'post_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
   def recent_comments
     comments.limit(5).order(created_at: :asc)
   end
