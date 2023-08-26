6413d00ac2b9c56e687284b5173a44df9f80c149
forLoop94 <charlesuwem94@gmail.com>
2023-08-26 12:33:40 +0100

    Add unit tests for likes model

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  let(:user) { User.create(name: 'Forloop', photo: 'myphoto', bio: 'My bio', post_counter: 0) }
+  let(:post) { Post.new(title: 'The importance of test', text: 'the content', author: user, likes_counter: 0, comments_counter: 0) }
+
+  it 'Should update the likes_count in post' do
+    Like.create(post:, author: user)
+    expect(post.likes.count).to eq(1)
+  end
+end
