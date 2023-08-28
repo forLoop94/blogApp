db88fdf0421ba79f315b9f300c8a26aafbd3c36c
forLoop94 <charlesuwem94@gmail.com>
2023-08-28 16:54:52 +0100

    Create route, action in appropriate controller and placeholder view file for /users/:id URL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,10 @@ class UsersController < ApplicationController
     @users = User.all
     render 'index'
   end
+
+  def show
+    @user = User.find(params[:id])
+    @recent_posts = @user.recent_post
+    render 'show'
+  end
 end
