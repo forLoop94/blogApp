e043da01d92999c5353f66c8d9381ff8592c868b
forLoop94 <charlesuwem94@gmail.com>
2023-08-28 17:02:10 +0100

    Create actual view file for /users/:id URL and remove placeholder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @recent_posts = @user.recent_post
+    @recent_posts = @user.recent_posts
     render 'show'
   end
 end
