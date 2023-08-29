88bb84eca9aed1926a77a59997afda57cfee1df5
forLoop94 <charlesuwem94@gmail.com>
2023-08-29 18:33:44 +0100

    replace have_rendered matcher with rspec-rails'render_template as required

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /index and /show' do
     it 'Should get all posts by user with id of 7' do
       get '/users/7/posts'
-      expect(response).to have_rendered('index')
+      expect(response).to render_template('index')
       expect(response.body).to include 'Pagination'
       expect(response).to have_http_status(:success)
     end
     it 'Should get the post with id of 1 by user with id of 7' do
       get '/users/7/posts/1'
-      expect(response).to have_rendered('show')
+      expect(response).to render_template('show')
       expect(response.body).to include 'Post #1 by username'
       expect(response).to have_http_status(:success)
     end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe 'Users', type: :request do
       get '/users'
       expect(response).to have_http_status(:success)
       expect(response.body).to include 'User Name'
-      expect(response).to have_rendered('index')
+      expect(response).to render_template('index')
     end
     it 'Should get details of the user with id of 7' do
       get '/users/7'
       expect(response).to have_http_status(:success)
       expect(response.body).to include 'See all posts'
-      expect(response).to have_rendered('show')
+      expect(response).to render_template('show')
     end
   end
 end
